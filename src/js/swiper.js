1ed41cbca6aa279fa2c908515adc757a14ba6b4d
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-01-23 15:30:16 -0300

    finish

---
+++ src/js/swiper.js
@@ -0,0 +1,32 @@
+
+
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    direction: 'horizontal',
+    loop: true,
+    preloadImages: true,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    autoplay: {
+      delay: 1000,
+    },
+  })
