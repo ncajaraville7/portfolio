02e9e64de5e48456613e69e2dbeae32a49e6bc3c
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2022-03-25 12:16:09 -0300

    fix desing

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,32 +1,30 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  direction: 'horizontal',
+  loop: true,
+  preloadImages: true,
 
-
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    direction: 'horizontal',
-    loop: true,
-    preloadImages: true,
-
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 40
-      }
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-
-    pagination: {
-      el: '.swiper-pagination',
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
-
-    autoplay: {
-      delay: 1000,
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-  })
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  autoplay: {
+    delay: 1000,
+  },
+});
